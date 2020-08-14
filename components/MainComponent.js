57c5c219059557688ff41756e6a6b1ee9dc9d3e7
Jaime Gerst <jaimegerst1@gmail.com>
2020-08-14 16:30:16 -0400

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import OptionsInfo from './OptionsInfoComponent';
 import { View, Platform } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator(
     {
@@ -23,11 +24,41 @@ const MenuNavigator = createStackNavigator(
     }
 );
 
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home }
+    },
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Menu: { screen: MenuNavigator }
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+);
+
 class Main extends Component {
     render() {
         return (
-            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
-                <MenuNavigator />
+            <View style={{
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight
+            }}>
+                <MainNavigator />
             </View>
         );
     }
--- shared/options.js
+++ shared/options.js
@@ -1,14 +1,7 @@
 export const OPTIONS = [
+    
     {
         id: 0,
-        name: "Home",
-        image: "/images/bagel.jpg",
-        elevation: 1233,
-        featured: false,
-        description: "Spreading love one bagel at a time."
-    },
-    {
-        id: 1,
         name: "Food",
         image: "/images/bagel.jpg",
         elevation: 1233,
@@ -16,7 +9,7 @@ export const OPTIONS = [
         description: "Check out our unique bagels and sandwiches, or our specialty of the month."
     },
     {
-        id: 2,
+        id: 1,
         name:"Drinks",
         image: "/images/coffee1.jpg",
         elevation: 877,
@@ -24,7 +17,7 @@ export const OPTIONS = [
         description: "Enjoy our fresh brewed coffe, teas, and juices."
     },
     {
-        id: 3,
+        id: 2,
         name:"Order Online!",
         image: "/images/onlineorder.jpg",
         elevation: 2901,
